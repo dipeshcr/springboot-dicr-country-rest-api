fb98155cb11534db9916501b3f4d5919d75936aa
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-21 00:12:56 +0200

    Novo - Adicao do Bean relativo ao ModelMapper

--- src/main/java/com/springboot/dicr/country/SpringbootDicrCountryRestApiApplication.java
+++ src/main/java/com/springboot/dicr/country/SpringbootDicrCountryRestApiApplication.java
@@ -1,10 +1,17 @@
 package com.springboot.dicr.country;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootDicrCountryRestApiApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDicrCountryRestApiApplication.class, args);
