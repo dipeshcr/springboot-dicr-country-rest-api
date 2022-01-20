fa7b0e5b2d8bf9e5c37da956f1180748690df284
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-20 19:34:26 +0200

    Criaccao da Estrutura do Projecto com.springboot.dicr.country.rest.api

---
+++ src/main/java/com/springboot/dicr/country/SpringbootDicrCountryRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.dicr.country;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDicrCountryRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDicrCountryRestApiApplication.class, args);
+	}
+
+}
