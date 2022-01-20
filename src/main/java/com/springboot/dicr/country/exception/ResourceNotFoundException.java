d460822d33b6bbf45c479b16728460f33a405945
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-21 00:16:46 +0200

    Novo - Criaccao de excepcao ResourceNotFoundException

---
+++ src/main/java/com/springboot/dicr/country/exception/ResourceNotFoundException.java
@@ -0,0 +1,39 @@
+package com.springboot.dicr.country.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * O Serial Version UID.
+	 */
+	private static final long serialVersionUID = -2391301585209584987L;
+
+	private String resourceName;
+
+	private String fieldName;
+
+	private Long fieldValue;
+
+	public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
+		super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldValue = fieldValue;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public Long getFieldValue() {
+		return fieldValue;
+	}
+
+}
