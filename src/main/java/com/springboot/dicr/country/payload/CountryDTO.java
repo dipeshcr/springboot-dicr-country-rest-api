22deb61847bc2560a044747969fe360f33f5fc0d
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-23 19:35:02 +0200

    Ajustes e Melhorias - Adicao de Validacao de Dados relacionados com Country

--- src/main/java/com/springboot/dicr/country/payload/CountryDTO.java
+++ src/main/java/com/springboot/dicr/country/payload/CountryDTO.java
@@ -1,5 +1,9 @@
 package com.springboot.dicr.country.payload;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.springboot.dicr.country.utils.BusinessMessages;
 
 import lombok.Data;
 
@@ -7,15 +11,24 @@ import lombok.Data;
 public class CountryDTO {
 
 	private Long id;
-	
+
+	@NotEmpty
+	@Size(min = 3, message = BusinessMessages.COUNTRY_NAME_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String name;
 
+	@NotEmpty
+	@Size(min = 3, message = BusinessMessages.COUNTRY_CAPITAL_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String capital;
 
+	@NotEmpty
+	@Size(min = 3, message = BusinessMessages.COUNTRY_REGION_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String region;
 
+	@NotEmpty
+	@Size(min = 3, message = BusinessMessages.COUNTRY_SUBREGION_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String subRegion;
 
+	@NotEmpty
 	private double area;
-	
+
 }
