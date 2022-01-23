ef89aa0c01d6951869f203bbc7313f3dedbd849a
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-24 00:11:56 +0200

    Ajustes e Melhorias - Adicao de documentacao Swagger

--- src/main/java/com/springboot/dicr/country/payload/CountryDTO.java
+++ src/main/java/com/springboot/dicr/country/payload/CountryDTO.java
@@ -5,29 +5,38 @@ import javax.validation.constraints.Size;
 
 import com.springboot.dicr.country.utils.BusinessMessages;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+@ApiModel(description = "Country Model Information")
 @Data
 public class CountryDTO {
 
+	@ApiModelProperty(value = "Country Id")
 	private Long id;
 
+	@ApiModelProperty(value = "Country Name")
 	@NotEmpty
 	@Size(min = 3, message = BusinessMessages.COUNTRY_NAME_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String name;
 
+	@ApiModelProperty(value = "Country Capital")
 	@NotEmpty
 	@Size(min = 3, message = BusinessMessages.COUNTRY_CAPITAL_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String capital;
 
+	@ApiModelProperty(value = "Country Region")
 	@NotEmpty
 	@Size(min = 3, message = BusinessMessages.COUNTRY_REGION_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String region;
 
+	@ApiModelProperty(value = "Country SubRegion")
 	@NotEmpty
 	@Size(min = 3, message = BusinessMessages.COUNTRY_SUBREGION_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS)
 	private String subRegion;
 
+	@ApiModelProperty(value = "Country Area")
 	@NotEmpty
 	private double area;
 
