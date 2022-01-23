19340dbcae1fe3214ce541203391e268c911c8c2
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-23 19:40:19 +0200

    Ajustes e Melhorias - Formatacao

--- src/main/java/com/springboot/dicr/country/utils/AppConstants.java
+++ src/main/java/com/springboot/dicr/country/utils/AppConstants.java
@@ -2,12 +2,12 @@ package com.springboot.dicr.country.utils;
 
 public class AppConstants {
 
-public static final String DEFAULT_PAGE_NUMBER="0";
-	
-	public static final String DEFAULT_PAGE_SIZE="10";
-	
-	public static final String DEFAULT_SORT_BY="id";
-	
-	public static final String DEFAULT_SORT_DIRECTION="asc";
-	
+	public static final String DEFAULT_PAGE_NUMBER = "0";
+
+	public static final String DEFAULT_PAGE_SIZE = "10";
+
+	public static final String DEFAULT_SORT_BY = "id";
+
+	public static final String DEFAULT_SORT_DIRECTION = "asc";
+
 }
