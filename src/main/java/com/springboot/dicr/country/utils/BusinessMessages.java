d214be74052c485d770a09624b30e34523affd9c
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-23 19:39:28 +0200

    Novo - Adicao de utilitario BusinessMessages

---
+++ src/main/java/com/springboot/dicr/country/utils/BusinessMessages.java
@@ -0,0 +1,22 @@
+package com.springboot.dicr.country.utils;
+
+public class BusinessMessages {
+
+	public static final String COUNTRY_NAME_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS = "Country name should have at least 3 characters";
+	
+	public static final String COUNTRY_CAPITAL_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS = "Country capital should have at least 3 characters";
+	
+	public static final String COUNTRY_REGION_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS = "Country region should have at least 3 characters";
+	
+	public static final String COUNTRY_SUBREGION_SHOULD_HAVE_AT_LEAST_THREE_CHARACTERS = "Country sub region should have at least 3 characters";
+
+	public static final String USERNAME_IS_ALREADY_TAKEN = "Username is already taken";
+	
+	public static final String EMAIL_IS_ALREADY_TAKEN = "Email is already taken";
+	
+	public static final String USER_PASSWORD_SHOULD_HAVE_AT_LEAST_SIX_CHARACTERS = "User Password should have at least 6 characters";
+
+	public static final String USER_NOT_FOUND_WITH_USERNAME_OR_EMAIL= "User not found with username or email ";
+
+	public static final String INVALID_JWT_SIGNATURE= "Invalid JWT Signature";
+}
