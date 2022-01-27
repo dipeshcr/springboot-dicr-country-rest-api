a29dc8aaa708366ce394b4141007538380c25d77
drogunata <dipeshrogunata_92@hotmail.com>
2022-01-27 23:16:55 +0200

    Ajustes e Melhorias - Adicao do metodo findByName()

--- src/main/java/com/springboot/dicr/country/repository/ICountryRepository.java
+++ src/main/java/com/springboot/dicr/country/repository/ICountryRepository.java
@@ -1,5 +1,7 @@
 package com.springboot.dicr.country.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.springboot.dicr.country.model.CountryEntity;
 
 @Repository
 public interface ICountryRepository extends JpaRepository<CountryEntity, Long> {
+
+	Optional<CountryEntity> findByName(final String name);
 
 }
